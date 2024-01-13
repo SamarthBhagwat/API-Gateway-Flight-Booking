bd37e66f04201fca940ed3eacc2e495fbdfd481b
Samarth <samarthrn19@gmail.com>
2024-01-13 18:38:59 +0530

    Added user service

---
+++ src/services/user-service.js
@@ -0,0 +1,31 @@
+const { UserRepository } = require('../repositories/')
+
+const userRepository = new UserRepository();
+
+class UserService{
+
+    async create(data){
+        try {
+            const user = await userRepository.create(data);
+            return user;    
+        } catch (error) {
+            console.log(error);
+            if(error.name == 'SequelizeValidationError' || error.name == 'SequelizeUniqueConstraintError'){
+                let error_array = error.errors;
+                let explanation = [];
+                error_array.forEach(element => {
+                    explanation.push(element.message);
+                });
+                const appError = new AppError(explanation, StatusCodes.BAD_REQUEST);
+                throw appError;
+            } 
+            else{
+                const appError = new AppError("Internal Server error", StatusCodes.INTERNAL_SERVER_ERROR);
+                throw appError;
+            }  
+        }
+    }
+
+}
+
+module.exports = UserService;
