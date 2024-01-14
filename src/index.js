6f84d1f75230f89c09557ae60f3ed002969d4975
Samarth <samarthrn19@gmail.com>
2024-01-15 02:42:21 +0530

    Implemented rate limiting in API gateway service

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 
 const app = express();
 
+
+const limiter = rateLimit({
+	windowMs: 2 * 60 * 1000, // 2 minutes
+	limit: 3, // Limit each IP to 3 requests per `window` (here, per 2 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Use an external store for consistency across multiple server instances.
+})
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(limiter);
 
 app.use('/api', apiRoutes);
 
