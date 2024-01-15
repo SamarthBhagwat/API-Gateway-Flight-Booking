12819b8c5c145010823c4869a86f3ec21fc8a6b5
Samarth <samarthrn19@gmail.com>
2024-01-15 23:53:59 +0530

    Implemented reverse proxy using http-proxy-middleware package

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const rateLimit = require('express-rate-limit');
+const {createProxyMiddleware} = require('http-proxy-middleware');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
@@ -19,8 +20,37 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(limiter);
 
+app.use('/flightservice' , createProxyMiddleware({
+    target: ServerConfig.FLIGHT_SERVICE, 
+    changeOrigin:true, 
+    pathRewrite: {'^/flightservice' : '/'}
+}));
+
+app.use('/bookingservice', createProxyMiddleware({
+    target: ServerConfig.BOOKING_SERVICE, 
+    changeOrigin:true, 
+    pathRewrite: {'^/bookingservice' : '/'}
+}));
+
 app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
 });
+
+
+/**
+ * METhod - 1
+ *  localhost:3000/api/v1/flights - one way is to expose this IP to the client, which we dont want to do 
+ * 
+ * Method - 2
+ * 
+ *  user -----> localhost:5000  ------>  localhost:3000/api/v1/flights (Flights service)
+ *                      |
+ *                      |
+ *                      -------------->  localhost:4000/api/v1/bookings (Booking service)
+ *  
+ *  localhost:5000/bookingService/api/v1/bookings --> forward it to booking service
+ *  localhost:5000/flightService/api/v1/flights  --->  forward it to flights service
+ * 
+ */
